1b8a7160d06b56fe1bf8930254e6a585d229cdda
Yana Reuchenko <yana@reuchenko.com>
2024-09-19 00:23:16 +0700

    Update

--- index.java
+++ index.java
@@ -1,8 +1,6 @@
 /******************************************************************************
 
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
+Test by Yana Reuchenko
 
 *******************************************************************************/
 
