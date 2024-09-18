e5714763559ce27bd2809283a56034d5e8113dc9
Yana Reuchenko <yana@Reuchenkos-MacBook-Air.local>
2024-09-19 00:15:57 +0700

    The furst commit

---
+++ index.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int a;
+	    Scanner in = new Scanner(System.in);
+        a = in.nextInt();
+        while (a>0){
+            
+            }
+		}
+}
